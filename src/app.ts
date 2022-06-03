d91f3b91ce489bb9eb71a206fd5df71ed64b1037
bbruno110 <bruno.santos6@ftc.edu.br>
2022-06-03 11:57:40 -0300

    teste 1

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express, { ErrorRequestHandler, Request, Response } from 'express';
 import cors from 'cors';
 import path from 'path';
-import router from './routes/rota'
+import routes from './routes/rota'
 
 
 const app =  express();
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(router);
+app.use('/', routes);
 app.use((req:Request, res:Response)=>{
     res.status(404);
     res.json({error: 'Endpoint não encontrado.'});
